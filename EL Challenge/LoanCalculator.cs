b83affa9b90a70f5d9e4f0a68009045d29757777
Pavel Khmelyov <pkhmelyov@gmail.com>
2018-08-30 01:56:17 +0300

    Implementation

---
+++ EL Challenge/LoanCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace EL_Challenge
+{
+    class LoanCalculator
+    {
+        public LoanCalculator(double amount, double interest, double downPayment, double term)
+        {
+            Amount = amount;
+            Interest = interest;
+            DownPayment = downPayment;
+            Term = term;
+        }
+
+        [JsonIgnore]
+        public double Amount { get; }
+        [JsonIgnore]
+        public double Interest { get; }
+        [JsonIgnore]
+        public double DownPayment { get; }
+        [JsonIgnore]
+        public double Term { get; }
+
+        [JsonIgnore]
+        public double MonthlyPayment
+        {
+            get
+            {
+                double amount = Amount - DownPayment;
+                double interest = Interest / 100 / 12;
+                double term = Term * 12;
+                return amount * (interest + interest / (Math.Pow(1 + interest, term) - 1));
+            }
+        }
+
+        [JsonIgnore]
+        public double TotalPayment { get => MonthlyPayment * Term * 12; }
+
+        [JsonIgnore]
+        public double TotalInterest { get => Math.Round(TotalPayment - Amount + DownPayment, 2); }
+
+        [JsonProperty("monthly payment", Order = 1)]
+        public double MonthlyPaymentRounded { get => Math.Round(MonthlyPayment, 2); }
+
+        [JsonProperty("total payment", Order = 3)]
+        public double TotalPaymentRounded { get => Math.Round(TotalPayment, 2); }
+
+        [JsonProperty("total interest", Order = 2)]
+        public double TotalInterestRounded { get => Math.Round(TotalInterest, 2); }
+    }
+}
--- EL Challenge/Program.cs
+++ EL Challenge/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using Newtonsoft.Json;
 
 namespace EL_Challenge
 {
@@ -10,6 +8,51 @@ namespace EL_Challenge
     {
         static void Main(string[] args)
         {
+            double? amount = null,
+                interest = null,
+                downPayment = null,
+                term = null;
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                string[] input = line.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                if (input.Length < 2) continue;
+                switch (input[0].Trim().ToUpperInvariant())
+                {
+                    case "AMOUNT":
+                        amount = GetAmount(input[1].Trim());
+                        break;
+                    case "INTEREST":
+                        interest = GetInterest(input[1].Trim());
+                        break;
+                    case "DOWNPAYMENT":
+                        downPayment = GetDownPayment(input[1].Trim());
+                        break;
+                    case "TERM":
+                        term = GetTerm(input[1].Trim());
+                        break;
+                }
+
+                if (amount.HasValue && interest.HasValue && downPayment.HasValue && term.HasValue)
+                {
+                    Console.ReadLine();
+                    break;
+                }
+            }
+
+            var calc = new LoanCalculator(amount.Value, interest.Value, downPayment.Value, term.Value);
+            Console.WriteLine(JsonConvert.SerializeObject(calc, Formatting.Indented));
         }
+
+        static double GetAmount(string input) => double.Parse(input, CultureInfo.InvariantCulture);
+
+        static double GetInterest(string input) => input.EndsWith("%") ?
+            double.Parse(input.TrimEnd('%'), CultureInfo.InvariantCulture) :
+            double.Parse(input, CultureInfo.InvariantCulture) * 100;
+
+        static double GetDownPayment(string input) => double.Parse(input, CultureInfo.InvariantCulture);
+
+        static double GetTerm(string input) => double.Parse(input, CultureInfo.InvariantCulture);
     }
 }
